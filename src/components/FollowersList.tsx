3d1df00e88807b97348292e4c589a61be9699dd9
Essential Randomness <essential.randomn3ss@gmail.com>
2023-05-07 15:08:08 -0700

    rate limit the shit out of this

--- src/components/FollowersList.tsx
+++ src/components/FollowersList.tsx
@@ -12,6 +12,10 @@ import clsx from "clsx";
 
 const { BskyAgent } = bsky;
 
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 const ratio = (profile: ProfileViewDetailed) => {
   if (!profile.followersCount) {
     return 1000;
@@ -43,7 +47,22 @@ const resumeSession = async () => {
   ).data;
 };
 
-const getFollowersData = async (user: { did: string }) => {
+const MAX_FOLLOWERS = 100;
+const checkMaxFollowers = async (user: { did: string }) => {
+  const profile = await AGENT.getProfile({ actor: user.did });
+  if (profile.data.followersCount! > MAX_FOLLOWERS) {
+    throw new Error(
+      "The BlueSky API rate limiter says you have too many followers."
+    );
+  }
+};
+
+const PEOPLE_FOR_ITERATION = 10;
+const getFollowersData = async (
+  user: { did: string },
+  progress: (chunk: number, total: number) => void
+) => {
+  await checkMaxFollowers(user);
   const followers: ProfileView[] = [];
   const data = (
     await AGENT.getFollowers({
@@ -53,26 +72,44 @@ const getFollowersData = async (user: { did: string }) => {
   followers.push(...data.followers);
   let cursor = data.cursor;
   while (cursor) {
-    const data = (
-      await AGENT.getFollowers({
-        actor: user.did,
-        cursor,
-      })
-    ).data;
-    followers.push(...data.followers);
-    cursor = data.cursor;
+    const result = await AGENT.getFollowers({
+      actor: user.did,
+      cursor,
+    });
+    if (!result.success) {
+      throw new Error("There was an error fetching the user data.");
+    }
+    result.data.followers.forEach((f) => followers.push(f));
+    cursor = result.data.cursor;
   }
+  const result: ProfileViewDetailed[] = [];
+  const iterations = Math.ceil(followers.length / PEOPLE_FOR_ITERATION);
 
-  return await Promise.all(
-    followers.map(
-      async (f) =>
-        (
+  for (let i = 0; i < iterations; i++) {
+    progress(i + 1, iterations);
+    console.log(`iteration ${i}`);
+    const requests = followers.slice(
+      i * PEOPLE_FOR_ITERATION,
+      (i + 1) * PEOPLE_FOR_ITERATION
+    );
+
+    const partialResult = await Promise.all(
+      requests.map(
+        async (f) =>
           await AGENT.getProfile({
             actor: f.did,
           })
-        ).data
-    )
-  );
+      )
+    );
+    if (partialResult.some((result) => !result.success)) {
+      throw new Error("There was an error fetching the user data.");
+    }
+    partialResult.forEach((r) => result.push(r.data));
+
+    await sleep(200 * PEOPLE_FOR_ITERATION);
+  }
+
+  return result;
 };
 
 const ratioSort = (a: ProfileViewDetailed, b: ProfileViewDetailed) =>
@@ -85,28 +122,37 @@ export default function () {
     ProfileViewDetailed[] | null
   >(null);
   const [hideMutuals, setHideMutuals] = React.useState<boolean>(false);
-  const [loading, setLoading] = React.useState<boolean>(false);
+  const [loading, setLoading] = React.useState<false | string>(false);
   const [algorithm, setAlgorithm] = React.useState<"ratio" | "following">(
     "ratio"
   );
+  const [error, setError] = React.useState<string | null>(null);
 
   const fetchData = async () => {
-    setLoading(true);
+    setLoading("Loading...");
     const user = await resumeSession();
-    setFollowers(await getFollowersData(user));
+    setFollowers(
+      await getFollowersData(user, (current, total) => {
+        setLoading(`Loading chunk ${current} of ${total}`);
+      })
+    );
     setLoading(false);
   };
 
   React.useEffect(() => {
-    fetchData();
+    fetchData().catch((e) => setError((e as Error).message));
   }, []);
 
   if (!getSession()) {
     return <></>;
   }
 
+  if (error) {
+    return <div className="text-red-600">{error}</div>;
+  }
+
   if (loading) {
-    return <div>Loading your followers...</div>;
+    return <div>{loading}</div>;
   }
 
   return (
